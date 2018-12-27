8b3ffe6482acd5b9589346d0fbff8076a9475355
duoyu <wangqh7812@163.com>
2018-12-27 09:01:24 +0800

    2018-12-27

--- Week_2/task_1_zhongyuan/scripts/footer.js
+++
@@ -1,6 +0,0 @@
-document.writeln("<div class='footer'>");
-document.writeln("   <div class='copyright'>");
-document.writeln("       <p>版权所有 @2009：香远（北京）投资有限公司 <br>");
-document.writeln("       技术支持：中远网络（北京）有限公司</p>");
-document.writeln("   </div>");
-document.writeln("</div>");

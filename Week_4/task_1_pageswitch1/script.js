9abe881ee8647141d5849ef3919e1785402c870f
duoyu <wangqh7812@163.com>
2019-03-21 17:40:13 +0800

    2019-03-21-week4-task1

---
+++ Week_4/task_1_pageswitch1/script.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    var oPic = document.getElementsByClassName("pic-placeholder").getElementsByTagName("img");
+    var aLi = document.getElementsByClassName("pic-list").getElementsByTagName("li");
+
+}

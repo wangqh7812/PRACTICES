0207f1c40956d2369e3bf28443b2a76694b93d2a
duoyu <wangqh7812@163.com>
2019-04-08 18:14:58 +0800

    2019-04-08-week4-彩票抽奖

--- Week_4/task_4_lottery/script.js
+++ Week_4/task_4_lottery/script.js
@@ -1,21 +1,60 @@
-var aLeftNum = document.getElementsByTagName("left-num");
+var oNumBox = document.getElementById("left-num-box");
+var aLeftNum = oNumBox.getElementsByTagName("li");
+
 var randomBtn = document.getElementById("left-btn-random");
 
+var aArr = [];
+var aRandomNum = [];
 
-
-//生成七个不重复的随机数
-function randomNum() {
-    var aRandomNum = [];
-    var j = 0;
-    while (j < 7) {
-        var temp = parseInt(Math.random() * 30 + 1);
-        if (aRandomNum.indexOf(temp) < 0) {
-            aRandomNum.push(temp);
-            j++;
-        }
-        console.log(aRandomNum);
-
-        return aRandomNum;
+//生成一个含有数字1-31的数组
+function addNumToArr() {
+    for (var i = 0; i < 31; i++) {
+        var temp = i + 1;
+        aArr.push(temp);
     }
+    return aArr;
 }
-randomNum();
+
+//从数组aArr随机选取七个数字
+function getRandomNum() {
+    // addNumToArr();
+    // console.log(aArr);
+    var a = 30;
+    for (var i = 0; i < 7; i++) {
+        var temp = Math.floor(Math.random() * a);
+        var b = aArr[temp];
+        // console.log(b);
+        aRandomNum.push(b);
+        aArr.splice(temp,1);
+        // console.log(aArr);
+        a--;
+    }
+    return aRandomNum;
+}
+
+// console.log(aArr);
+// console.log(aRandomNum);
+
+randomBtn.onclick = function () {
+    console.log(aRandomNum.length);
+    if (aRandomNum.length > 0) {
+        aRandomNum.splice(0,aRandomNum.length);
+    }
+    // console.log(aRandomNum.length,aArr.length);
+    getRandomNum();
+    // console.log(aLeftNum.length,aRandomNum);
+    for (var i = 0; i < 31; i++) {
+        aLeftNum[i].index = i;
+        // console.log(aLeftNum[i]);
+        var temp = aLeftNum[i].innerText;
+        // console.log(temp);
+        for (var j = 0; j < 7; j++) {
+            // console.log(aRandomNum[j]);
+            if ( aRandomNum[j] === temp) {
+                aLeftNum[i].className = "left-num num-selected";
+            }
+        }
+    }
+};
+
+

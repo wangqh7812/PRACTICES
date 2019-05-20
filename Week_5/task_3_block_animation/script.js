66330c06b96dab83d473fc51a046b66cda5aa72c
duoyu <wangqh7812@163.com>
2019-05-20 17:36:32 +0800

    2019-05-20-块大小左右的变化

--- Week_5/task_3_block_animation/script.js
+++ Week_5/task_3_block_animation/script.js
@@ -3,29 +3,39 @@ var outWrap = document.getElementsByClassName("in-wrap");
 
 var index = 0;
 
-var speedIn,
-    speedOut;
+var speedOut;
 
-var timerIn = null,
-    timerOut = null;
+var timerOut = null;
 
 for (var i = 0; i < boxList.length; i++){
     boxList[i].index = i;
     boxList[i].onclick = function () {
+        var s = this.offsetLeft;
         console.log("当前的box的offsetLeft：" + this.offsetLeft);
+        bodyAnimation(this);
+        posAnimation(s);
     }
 }
 
-function posAnimation() {
+function posAnimation(long) {
 
-    if (timerOut) {
-        clearInterval(timerOut);
+    // if (timerOut) {
+    //     clearInterval(timerOut);
+    // }
+    //
+    // timerOut = setInterval(function () {
+    //     if (temp > 0) {
+    //         nowPos = nowPos - speedOut;
+    //     }
+    //
+    // })
+}
+
+function bodyAnimation(ele) {
+    for (var j = 0; j < boxList.length; j++) {
+        boxList[j].style.width = "100px";
+        boxList[j].style.height = "200px";
     }
-
-    timerOut = setInterval(function () {
-        if (temp > 0) {
-            nowPos = nowPos - speedOut;
-        }
-
-    })
+    ele.style.width = "200px";
+    ele.style.height = "400px";
 }

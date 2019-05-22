989aa8bc9497467e3c7c7d84d7fb8ded4cf864c1
duoyu <wangqh7812@163.com>
2019-05-22 17:35:49 +0800

    2019-05-22-块大小左右的变化

--- Week_5/task_3_block_animation/script_CSS3.js
+++ Week_5/task_3_block_animation/script_CSS3.js
@@ -3,15 +3,16 @@ var outWrap = document.getElementsByClassName("in-wrap");
 
 var index = 0;
 
-var speedOut;
-
-var timerOut = null;
+// var speedOut;
+//
+// var timerOut = null;
 
 for (var i = 0; i < boxList.length; i++){
     boxList[i].index = i;
     boxList[i].onclick = function () {
         var a = this.index;
-        console.log("当前的box的offsetLeft：" + this.offsetLeft);
+        // console.log("当前的box的offsetLeft：" + this.offsetLeft);
+        // outWrap[0].className = "in-wrap pos" + a;
         posAnimation(a);
         bodyAnimation(this);
 
@@ -19,16 +20,7 @@ for (var i = 0; i < boxList.length; i++){
 }
 
 function posAnimation(index) {
-    for(var i = 0; i < boxList.length; i++) {
-        boxList[i].index = i;
-        if (boxList[i].className == "box active") {
-            var a = this.index;
-        }
-        if (a < index) {
-
-
-        }
-    }
+    outWrap[0].className = "in-wrap pos" + index;
 }
 
 function bodyAnimation(ele) {
